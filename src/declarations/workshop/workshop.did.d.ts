02308c00d2c8922e1e8bd704e312dc542d75ef5e
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-03-29 01:23:48 +0200

    REMOVE declarations

--- src/declarations/workshop/workshop.did.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export type Command = { 'initCommand' : { 'value' : Entity } };
-export type Entity = bigint;
-export type Event = { 'initEvent' : { 'value' : Entity } };
-export type List = [] | [[Event, List]];
-export interface State { 'value' : Entity, 'events' : List }
-export interface _SERVICE {
-  'action' : (arg_0: Command) => Promise<[] | [State]>,
-}
